86ab6da3936391870d4fa44de0c9a6cfc7e7d619
Domenico Mammola <mimmo71@gmail.com>
2022-07-18 18:22:01 +0200

    * learning...

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,72 @@ import logo from './logo.svg';
 import './App.css';
 import SearchFormB from './SearchFormB';
 import SearchForm from './SearchForm';
-import Stock, { StockProps } from './Stock';
 import SearchFormHook from './SearchFormHook';
+import StockOld from './StockOld';
+import Stock, { StockProps, StockQuotationProps } from './Stock';
 
 // https://github.com/typescript-cheatsheets/react#reacttypescript-cheatsheets
 
 type AppState = {
   showMsg: boolean,
-  maggiorenne: string
+  maggiorenne: string,
+  listaStock: Array<StockQuotationProps>
 }
 
 class App extends Component<any, AppState> {
   state: AppState = {
     showMsg: false,
-    maggiorenne: ''
+    maggiorenne: '',
+    listaStock: []
   }
-  mostroMessaggio = (evento : any) => {
-    const valore : boolean = ! this.state.showMsg;
-    this.setState({showMsg : valore});
+  mostroMessaggio = (evento: any) => {
+    const valore: boolean = !this.state.showMsg;
+    this.setState({ showMsg: valore });
   }
 
-  showMaggiorenne = (nome : string) => {
-    this.setState({maggiorenne: nome});
+  showMaggiorenne = (nome: string) => {
+    this.setState({ maggiorenne: nome });
+  }
+
+  aggiornoStock = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const newStock = {nome: 'AMZN', quotazione : 1200};
+    this.setState({listaStock: [newStock]});
+
+  }
+
+  constructor(props: any) {
+    super(props);
+    //this.state.listaStock = [{ nome: 'APPL', quotazione: 250 }];
+    console.log('1g) GENITORE: creo istanza');
+  }
+
+  static getDerivedStateFromProps(np: any, ps: any) {
+    console.log('1ga) GENITORE check props e state');
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('3g) GENITORE: DidMount');
+    const quot: StockQuotationProps =  { nome: 'MAZI', quotazione: 350 };    
+    const stock = [quot];
+    console.log(this.state.listaStock);
+    this.setState({listaStock: stock}); //state.listaStock.push(quot);
+    //this.state.listaStock.push(quot);
+    console.log(this.state.listaStock);
+    //this.setState({ maggiorenne: 'CICCIO' });
   }
 
   render() {
+    console.log('2g) GENITORE: render');
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header">          
+          <p>Applicazione stock quote</p>
+          <button onClick={this.aggiornoStock}>TopStock</button>
+          {this.state.listaStock.map(el => <Stock key={el.nome} datistock={el} />)}
+
+          {/*
           <p>
             <SearchFormHook></SearchFormHook>
           </p>
@@ -39,13 +77,15 @@ class App extends Component<any, AppState> {
           </p>
           {this.state.showMsg && <p>Messaggio nascosto</p>}
           
-          {/* <SearchFormB ></SearchFormB> */}
+           <SearchFormB ></SearchFormB> 
           <p>
             <SearchForm></SearchForm>
           </p>
           <p>
-            <Stock nome={"Apple"} eta={14} showEta={this.showMaggiorenne}></Stock>
-          </p>
+            <StockOld nome={"Apple"} eta={14} showEta={this.showMaggiorenne}></StockOld>
+          </p>          
+
+          */}
           <p>Maggiorenne: {this.state.maggiorenne}</p>
         </header>
       </div>
--- src/Stock.tsx
+++ src/Stock.tsx
@@ -1,38 +1,46 @@
+import { stat } from 'fs';
 import React from 'react'
+import { threadId } from 'worker_threads';
 
-export interface StockProps {nome : String, eta: number, showEta? : any}
+export interface StockQuotationProps { nome: string, quotazione: number };
+export interface StockProps {datistock: StockQuotationProps};
 
-type StockState = {
-    nome : String,
-    eta : number,
-    showEta : any
+type StockState = {    
+    quotazione: number;
 }
 
-export default class Stock extends React.Component <StockProps, StockState> {    
-    state : StockState = {
-        nome : this.props.nome,
-        eta : this.props.eta,
-        showEta : this.props.showEta
+export class Stock extends React.Component<StockProps, StockState> {
+    state : StockState = {        
+        quotazione: this.props.datistock.quotazione
     }
-    aggiornoStato = () => {
-        const nuovaEta : number = this.state.eta + 1;
-        this.setState({eta : nuovaEta});
-        console.log ("nuova età:" + nuovaEta);
-        if (nuovaEta >= 18) {
-            this.state.showEta(this.state.nome);
+
+    constructor (props : StockProps){
+        super(props);        
+        console.log('1f) FIGLIO - Creo istanza ' + this.props.datistock.nome);
+    }
+    componentDidMount(){
+        console.log('3f) FIGLIO - DidMount ' + this.props.datistock.nome);
+    }
+    componentDidUpdate(prevProps : StockProps, prevState: StockState){
+        console.log('4f) FIGLIO - DidUpdate ' + this.props.datistock.nome);
+        if (prevProps.datistock.quotazione !== this.props.datistock.quotazione) {
+            this.setState((state : StockState, props: StockProps) => ({quotazione: props.datistock.quotazione}));
         }
     }
-    realTime = () => {
-        setInterval(()=>(this.aggiornoStato()), 3000);
+    aggiornoStock = () => {
+        console.log('aggiornoStock');
+        this.setState((state: StockState, props: StockProps)  => ({quotazione: state.quotazione + 10 }));
     }
-  render() {
-    return (
-      <div>
-          <p>
-          Stock {this.state.nome} età {this.state.eta}
-          </p>
-          <button onClick={this.realTime}>Aggiorna</button>
-      </div>
-    )
-  }
+    render() {
+        console.log('2f) FIGLIO - render ' + this.props.datistock.nome);
+        return (
+            <div>
+                <p>{this.props.datistock.nome}</p>
+                <p>{this.state.quotazione}</p>
+                <button onClick={this.aggiornoStock}>AGGIORNA</button>
+                </div>
+        )
+    }
 }
+
+export default Stock
